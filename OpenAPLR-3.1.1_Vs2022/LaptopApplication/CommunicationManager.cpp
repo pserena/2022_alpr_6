791048ad4d64cd46dd02c6e624965421baa116c8
sskymirr <sskymirr@gmai.com>
2022-07-14 22:24:25 +0900

    vehicleInfo add

--- OpenAPLR-3.1.1_Vs2022/LaptopApplication/CommunicationManager.cpp
+++ OpenAPLR-3.1.1_Vs2022/LaptopApplication/CommunicationManager.cpp
@@ -9,7 +9,7 @@ TTcpConnectedPort* TcpConnectedPort;
 
 using namespace client;
 client::ResponseMode GetResponseMode = client::ResponseMode::ReadingHeader;
-static void GetResponses(char* data);
+static int GetResponses(char* data);
 size_t RespHdrNumBytes;
 unsigned int BytesInResponseBuffer = 0;
 ssize_t BytesNeeded = sizeof(RespHdrNumBytes);
@@ -57,26 +57,31 @@ int CommunicationManager::sendCommunicationData(unsigned char* data) {
 
 int CommunicationManager::receiveCommunicationData(char* data)
 {
-    GetResponses(data);
+    int result = GetResponses(data);
+    if (result < 0)
+        retryNetworkConnect();
     return 0;
 }
 
-static void GetResponses(char* data)
+static int GetResponses(char* data)
 {
     char* ResponseBuffer = data;
     ssize_t BytesRead;
     ssize_t BytesOnSocket = 0;
     while ((BytesOnSocket = BytesAvailableTcp(TcpConnectedPort)) > 0)
     {
-        if (BytesOnSocket < 0) return;
+        if (BytesOnSocket < 0) return -1;
         if (BytesOnSocket > BytesNeeded) BytesOnSocket = BytesNeeded;
         BytesRead = ReadDataTcp(TcpConnectedPort, (unsigned char*)&ResponseBuffer[BytesInResponseBuffer], BytesOnSocket);
         if (BytesRead <= 0)
         {
             printf("Read Response Error - Closing Socket\n");
-            CloseTcpConnectedPort(&TcpConnectedPort);
+            //CloseTcpConnectedPort(&TcpConnectedPort);
+            return -1;
         }
-        BytesInResponseBuffer += BytesRead;
+        else {
+            BytesInResponseBuffer += BytesRead;
+        }
 
         if (BytesInResponseBuffer == BytesNeeded)
         {
@@ -100,10 +105,38 @@ static void GetResponses(char* data)
     if (BytesOnSocket < 0)
     {
         printf("Read Response Error - Closing Socket\n");
-        CloseTcpConnectedPort(&TcpConnectedPort);
+        //CloseTcpConnectedPort(&TcpConnectedPort);
+        return -1;
     }
+    return 0;
 }
 
 int CommunicationManager::authenticate(string strID, string strPw) {
+    auto jsonMessageLogin = R"(
+        {
+            "request_type": "login"
+        }
+    )"_json;
+    jsonMessageLogin["user_id"] = "daniel";//strID.c_str();
+    jsonMessageLogin["user_password"] = "1qaz2wsx";// strPw.c_str();
+    string messageLogin = jsonMessageLogin.dump();
+    printf("Response %s\n", messageLogin.c_str());
+    sendCommunicationData((unsigned char* )messageLogin.c_str());
+
 	return 0;
+}
+
+int CommunicationManager::sendRecognizedInfo(string rs, int puid) {
+    auto jsonMessage = R"(
+        {
+            "request_type": "query"
+        }
+    )"_json;
+    jsonMessage["plate_number"] = rs.c_str();
+    jsonMessage["plate_uid"] = to_string(puid);
+    string messageRecognized = jsonMessage.dump();
+    printf("Response %s\n", messageRecognized.c_str());
+    sendCommunicationData((unsigned char*)messageRecognized.c_str());
+
+    return 0;
 }
--- OpenAPLR-3.1.1_Vs2022/LaptopApplication/VehicleInfoManager.cpp
+++ OpenAPLR-3.1.1_Vs2022/LaptopApplication/VehicleInfoManager.cpp
@@ -1,6 +1,9 @@
 #include "LapTopModules.h"
+#include <thread>
 
 using namespace client;
+
+static int receiveThread(VehicleInfoManager* vehicleMan);
 
 VehicleInfoManager::VehicleInfoManager(void) {
 }
@@ -14,6 +17,11 @@ int VehicleInfoManager::linkCommMag(CommunicationManager* linkCommMan) {
 	return 0;
 }
 
+int VehicleInfoManager::VehicleInfoReceiveStart(void) {
+	recevieThreadStart();
+	return 0;
+}
+
 int VehicleInfoManager::sendVehicleInfo(unsigned char* vehicleData) {
 	printf("sendVehicleInfo ->%s\n", vehicleData);
 	commMan->sendCommunicationData(vehicleData);
@@ -21,15 +29,46 @@ int VehicleInfoManager::sendVehicleInfo(unsigned char* vehicleData) {
 	return 0;
 }
 
-int VehicleInfoManager::receiveCommunicationData(char* vehicleData) {
-	commMan->receiveCommunicationData(vehicleData);
+int VehicleInfoManager::setRecognizedInfo(string rs, int puid, Mat pimag)
+{
+	printf("rs:%s puid:%d\n", rs, puid);
+	commMan->sendRecognizedInfo(rs, puid);
 
 	return 0;
 }
 
-int VehicleInfoManager::setRecognizedInfo(string rs, int puid, Mat pimag)
+int VehicleInfoManager::receiveCommunicationData(void)
 {
-	printf("rs:%s puid:%d\n", rs, puid);
+	char ResponseBuffer[8192];
+	int result = commMan->receiveCommunicationData(ResponseBuffer);
+	//if (result < 0)
+	//    commMan->retryNetworkConnect();
+	return 0;
+}
 
+void VehicleInfoManager::recevieThreadStart(void) {
+	static bool startThread = false;
+	if (!startThread) {
+		timer_start(receiveThread, 20);
+		startThread = true;
+	}
+}
+
+void VehicleInfoManager::timer_start(std::function<void(VehicleInfoManager*)> func, unsigned int interval)
+{
+	std::thread([func, this, interval]() {
+		while (true)
+		{
+			func(this);
+			std::this_thread::sleep_for(std::chrono::milliseconds(interval));
+		}
+		}).detach();
+}
+
+static int receiveThread(VehicleInfoManager* vehicleMan)
+{
+	char ResponseBuffer[8192];
+	vehicleMan->receiveCommunicationData();
+	//printf("receiveThreadt\n");
 	return 0;
 }
